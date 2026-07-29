495d8cb5e63ed04b1a689b9edaaf0b09c82a4e22
sai karthik_sai <nalamsaikarthik@gmail.com>
2026-07-29 22:38:18 +0530

    Update bank balance from 700000 to 810000

--- Threads/BankDemo.java
+++ Threads/BankDemo.java
@@ -1,5 +1,5 @@
 ouclass Customer extends Thread {
-    static int balance = 700000;
+    static int balance = 810000;
     private final int withdrawAmount;
 
     public Customer(String name, int amount) {
