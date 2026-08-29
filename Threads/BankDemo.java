6444ee79c7a4f0291308c6cc0e23177261a81786
sai karthik_sai <nalamsaikarthik@gmail.com>
2026-08-29 23:21:19 +0530

    Change initial balance from 30000 to 20000

--- Threads/BankDemo.java
+++ Threads/BankDemo.java
@@ -1,5 +1,5 @@
 ouclass Customer extends Thread {
-    static int balance = 30000;
+    static int balance = 20000;
     private final int withdrawAmount;
 
     public Customer(String name, int amount) {
